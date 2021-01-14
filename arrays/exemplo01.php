815504838695e2bf9c4183080503480abc37f3cb
Thiago Vieira <thiagoviera0007@yahoo.com.br>
2021-01-14 19:13:49 -0300

    arrays/Ataque_a_sessores

---
+++ arrays/exemplo01.php
@@ -0,0 +1,8 @@
+<?php
+
+$frutas = array("laranja", "abacaxi", "morango", "melancia");
+
+print_r($frutas);
+
+
+ ?>
---
+++ arrays/exemplo03.php
@@ -0,0 +1,15 @@
+<?php
+$pessoas = array();
+
+array_push($pessoas,array(
+  'nome' =>'Thiago' ,
+  'idade' => 30
+));
+array_push($pessoas,array(
+  'nome' =>'Cezar' ,
+  'idade' => 25
+));
+
+print_r($pessoas[0]);
+
+ ?>
