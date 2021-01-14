d8ec09c597651cba9d9af5e41aaee268cba6ffcb
Thiago Vieira <thiagoviera0007@yahoo.com.br>
2021-01-14 19:43:01 -0300

    data

---
+++ data/exemplo1.php
@@ -0,0 +1,12 @@
+<?php
+// datas funso horarios e afins
+
+echo date("d/m/Y H:i:s");
+
+echo "<br>";
+echo "<hr>";
+
+echo time(); // a função time retorna os segundos desde primeiro de janeiro de 1970 até agora
+
+
+?>
