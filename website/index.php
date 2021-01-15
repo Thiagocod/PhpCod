8a91944c3e9588d0fc50648331d784360f03060b
Thiago Vieira <thiagoviera0007@yahoo.com.br>
2021-01-14 22:55:52 -0300

    tplWeb

---
+++ website/index.php
@@ -0,0 +1,29 @@
+<?php
+
+  require_once("vendor/autoload.php");
+
+  $app = new \Slim\Slim();
+
+  $app->get('/', function(){
+
+    echo "Home page";
+
+    echo "<br>";
+
+    echo json_encode(array(
+      'date'=>date('Y-m-d H:i:s')
+    ));
+
+  });
+
+  $app->get('/hello/:name',function($name){
+
+    echo "Hello, ".$name;
+
+
+  });
+
+ $app->run();
+
+
+?>
